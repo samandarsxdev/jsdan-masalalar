3fe458aa1981a32d4b71fa7d33dacd7356ea5045
samandarsxdev <93664571+samandarsxdev@users.noreply.github.com>
2021-11-17 13:37:17 -0500

    add

---
+++ main.js
@@ -0,0 +1,25 @@
+// var age = prompt("A ga qiymat kiriting");
+// var num1 = Math.pow(age,2);
+// var num2 = Math.pow(age,4);
+// var num3 = Math.pow(age,8);
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+// var gradus=prompt("gradusga otkazish uchun qiymat bering");
+// const pi=180;
+// var res=gradus/pi;
+// console.log(res);
+
+// var LUzunlik=prompt("uzunlik olchamini kiriting(sm)");
+// var res=LUzunlik/100;
+// console.log(`uzuznlik ${res} metrda
+// `);
+
+var number=prompt("ikki xonali son kiriting");
+console.log(number);
+var arr=[];
+arr=number;
+// var num= arr==number;
+var res= arr[0]+arr[1];
+console.log(res);
